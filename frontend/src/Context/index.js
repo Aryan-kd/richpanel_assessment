f755f0b965e9566f6f17f3992235ee4fdd6e9e04
Aryan Kodinya <aryan.kodinya@gmail.com>
2023-08-12 16:02:04 +0530

    Api Access point fixed

--- frontend/src/Context/index.js
+++ frontend/src/Context/index.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from 'react';
 import axios from 'axios';
+// const url = 'https://richpanelapi.onrender.com:10000';
 const url = 'http://localhost:4000';
 
 const ContextApi = createContext();
